63d4f086996d0444725778b7ada8908438ea18e2
Rence Valerio <rmvalerio@myseneca.ca>
2026-05-15 20:32:45 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Author: Rence Michael Valerio
+#Author ID: rmvalerio
+#Date Created: 2026/05/
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
