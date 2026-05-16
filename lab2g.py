8f1e3bd413bce43acc666f740e2adb70fe395097
Rence Valerio <rmvalerio@myseneca.ca>
2026-05-15 20:32:55 -0400

    Added lab2g

---
+++ lab2g.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#Author: Rence Michael Valerio
+#Author ID: rmvalerio
+#Date Created: 2026/05/
+
+import sys
+
+timer = 3
+
+if len(sys.argv) > 1 :
+    timer = int(sys.argv[1])
+    while timer != 0:
+        print (timer)
+        timer = timer - 1
+    print('blast off!')
+
+else:
+    while timer != 0:
+        print (timer)
+        timer = timer - 1
+    print('blast off!')
